cd2304871d6bad82bef9c05d6872ae9dde59541f
Lakshit Kataria <lakshitkataria69@gmail.com>
2025-10-04 16:10:55 +0530

    Initial commit

---
+++ src/com/mycompany/project/inhertinace.java
@@ -0,0 +1,31 @@
+package com.mycompany.project;
+
+class animal{
+
+    String name;
+    void eat(){
+        System.out.println(name + " is eating");
+    }
+
+     void bark() {
+        System.out.println(name + " is barking");
+
+    }
+}
+
+class dog extends animal{
+}
+
+ class inhertinace{
+     public static void main (String[] args){
+        dog mydog= new dog();
+
+        mydog.name="doggy";
+
+        mydog.bark();
+
+        mydog.eat();
+
+    }
+
+}
